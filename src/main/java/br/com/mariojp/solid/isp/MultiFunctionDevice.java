2e584dfa72e6055f845db02686ed577b49b5683b
pennA <lucaspsantos003@gmail.com>
2025-08-31 12:47:26 -0300

    feature:valida que printReport não chama scan desnecessariamente

--- src/main/java/br/com/mariojp/solid/isp/MultiFunctionDevice.java
+++ src/main/java/br/com/mariojp/solid/isp/MultiFunctionDevice.java
@@ -1,6 +1,5 @@
 package br.com.mariojp.solid.isp;
 
-public interface MultiFunctionDevice {
-	void print(String content);
-	void scan(String targetFile);
+public interface MultiFunctionDevice extends iPrinter,iScan {
+	
 }
--- src/main/java/br/com/mariojp/solid/isp/OfficeService.java
+++ src/main/java/br/com/mariojp/solid/isp/OfficeService.java
@@ -1,11 +1,15 @@
 package br.com.mariojp.solid.isp;
 
 public class OfficeService {
-    private final MultiFunctionDevice device;
-    public OfficeService(MultiFunctionDevice device){ this.device = device; }
-    public void printReport(String content){
-        device.print(content);
-        // BUG: chama scan sem necessidade
-        device.scan("audit.txt");
+    private iPrinter printer;
+
+    public OfficeService(iPrinter printer) {
+        this.printer = printer;
     }
+
+    public void printReport(String report) {
+        printer.print(report);
+
+    }
+
 }
